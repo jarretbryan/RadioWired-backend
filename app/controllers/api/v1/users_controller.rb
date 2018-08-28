ce0ca8453350273f285fc4f1215c6fdcc99f54b1
jarretbryan <jarret.bryan@gmail.com>
2018-08-28 15:09:34 -0400

    edit user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::UsersController < ApplicationController
+
+    before_action :get_user, only: [:show, :update, :destroy]
+
+    # Not for use in front end client
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+
+    def create
+        @user = User.find_or_create_by(user_params)
+        if @user.save
+            render json: @user, status: :accepted
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        @user.delete 
+    end
+
+
+    private
+
+    def get_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:email, :password_digest, :username, :password_digest)
+    end
+
 end

bc9cba15a91ab4f667d1331e214d9bb01d8d2b2f
jarretbryan <jarret.bryan@gmail.com>
2018-08-30 15:54:32 -0400

    Added email formatting for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     
     validates_presence_of :email, :password_digest
-    validates :email, uniqueness: true
+    validates :email, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
     has_secure_password
     
 

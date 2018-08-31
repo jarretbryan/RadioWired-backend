439ea6961a6973a7f24f6a74847cfe9e1303d376
jarretbryan <jarret.bryan@gmail.com>
2018-08-30 20:51:56 -0400

    begin building service object to talk to listennotes

---
+++ app/services/stream_from_genre_service.rb
@@ -0,0 +1,30 @@
+require 'json'
+## test run for genre
+
+class StreamFromGenre
+  def initialize(params)
+    @playlist = params[:playlist]
+  end
+
+  # i have to iterate through an object that has all the key value pairs for genre, and pass that into my response below
+
+    
+    response = Unirest.get "https://listennotes.p.mashape.com/api/v1/best_podcasts?genre_id=133&page=2",
+    headers:{
+      "X-Mashape-Key" => ENV['X-MASHAPE-KEY'],
+      "X-Mashape-Host" => ENV['X-MASHAPE-HOST']
+    }
+    
+    # genre_info here is a string (stringified hash)
+    genre_info = response.raw_body
+    
+    # get JSON
+    genre_hash = JSON.parse(genre_info)
+    
+    list_of_relevant_podcasts = genre_hash["channels"]
+    
+    
+    # dont do request
+    
+  end
+    

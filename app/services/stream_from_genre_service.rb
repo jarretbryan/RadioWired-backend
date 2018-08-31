176bc3ed986664f4d28c8adca26bae49ee687486
jarretbryan <jarret.bryan@gmail.com>
2018-08-31 09:56:42 -0400

    consider api calls

--- app/services/stream_from_genre_service.rb
+++ app/services/stream_from_genre_service.rb
@@ -1,7 +1,7 @@
 require 'json'
 ## test run for genre
 
-class StreamFromGenre
+class ListenNotes
   # def initialize(params)
   #   @playlist = params[:playlist]
   # end
@@ -10,6 +10,26 @@ class StreamFromGenre
   # might refactor to make this an adapter object, not a service object 
 
   # i have to iterate through an object that has all the key value pairs for genre, and pass that into my response below
+
+  def get_genre_pairs
+    response = Unirest.get "https://listennotes.p.mashape.com/api/v1/genres",
+    headers:{
+      "X-Mashape-Key" => ENV['X-MASHAPE-KEY'],
+      "X-Mashape-Host" => ENV['X-MASHAPE-HOST']
+    }
+
+    genre_array = JSON.parse(response.genres)
+
+  end
+
+
+  # I have to pass in an object with the key genre
+  def get_stream_by_genre
+    def def initialize(params)
+      @genre_string = params[:genre]
+    end
+    
+  
 
     
     response = Unirest.get "https://listennotes.p.mashape.com/api/v1/best_podcasts?genre_id=133&page=2",

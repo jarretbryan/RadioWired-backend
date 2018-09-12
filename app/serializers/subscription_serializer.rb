9324660aca59025c4d8aca3c63ca499de9f6e781
jarretbryan <jarret.bryan@gmail.com>
2018-09-12 15:23:11 -0400

    edit serializers to allow for deletion

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -2,4 +2,5 @@ class SubscriptionSerializer < ActiveModel::Serializer
   attributes :id, :website, :publisher, :title, :country, :image, :rss, :language, :thumbnail, :description, :ep_id
 
   belongs_to :playlist
+  has_many :favorites
 end

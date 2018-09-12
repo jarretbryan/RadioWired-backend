6b75911148fc1d9b0c3333ee5aba94150419c73a
jarretbryan <jarret.bryan@gmail.com>
2018-09-12 14:40:32 -0400

    show created_at in playlist serializer - explicitly add new subscriptions to last playlist

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,5 +1,5 @@
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :genre, :user_id
+  attributes :id, :title, :description, :genre, :user_id, :created_at
 
   has_many :subscriptions
   belongs_to :user
